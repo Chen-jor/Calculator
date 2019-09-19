d9744c53792f3477f7554d795bf181d6bcd60235
981323032@qq.com <981323032@qq.com>
2019-09-19 15:05:09 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Conso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
